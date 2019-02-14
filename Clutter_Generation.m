0230134bcc9b042f05159afa3864104cd9c3b6ae
Venkata Sasikiran Veeramachaneni <vveeram2@gmu.edu>
2019-02-13 21:22:30 -0500

    added the linear clutter generation in the cullter gen function to run  another set of simulations

--- Clutter_Generation.m
+++ Clutter_Generation.m
@@ -1,16 +1,27 @@
  function ClutterStruct = Clutter_Generation(TarMaxRho,clutterDensity,impInstVec)
     
-    areaSquare = (2*TarMaxRho)^2;
-    ptsGen = ceil(areaSquare*clutterDensity);
+%     areaSquare = (2*TarMaxRho)^2;
+%     ptsGen = ceil(areaSquare*clutterDensity);
+%     n = size(impInstVec,1);
+%     xClutter = -TarMaxRho + 2*TarMaxRho*rand(ptsGen,n);          %This is uniform distributiom
+%     yClutter = -TarMaxRho + 2*TarMaxRho*rand(ptsGen,n);          %This is uniform distributiom
+%     
+%     for i = n:-1:1
+%         [thetaClutter,rhoClutter] = cart2pol(xClutter(:,i),yClutter(:,i));
+%         indx = find(rhoClutter<=TarMaxRho);
+%         ClutterStruct(i).thetaClutter = thetaClutter(indx,:);
+%         ClutterStruct(i).rhoClutter = rhoClutter(indx,:);
+%         noClutterPts = length(indx);        
+%         ClutterStruct(i).rangeRateClutter = 5*randn(noClutterPts,1);
+%     end
+    area = pi*TarMaxRho^2;
+    points = ceil(area*clutterDensity);
     n = size(impInstVec,1);
-    xClutter = -TarMaxRho + 2*TarMaxRho*rand(ptsGen,n);          %This is uniform distributiom
-    yClutter = -TarMaxRho + 2*TarMaxRho*rand(ptsGen,n);          %This is uniform distributiom
-    
-    for i = n:-1:1
-        [thetaClutter,rhoClutter] = cart2pol(xClutter(:,i),yClutter(:,i));
-        indx = find(rhoClutter<=TarMaxRho);
-        ClutterStruct(i).thetaClutter = thetaClutter(indx,:);
-        ClutterStruct(i).rhoClutter = rhoClutter(indx,:);
-        noClutterPts = length(indx);        
-        ClutterStruct(i).rangeRateClutter = 5*randn(noClutterPts,1);
+    thetaClutter = 2*pi*rand(points,n);                                %This is uniform distributiom
+    rhoClutter = TarMaxRho*rand(points,n);                               %This is uniform distributiom
+    rangeRateClutter = 5*randn(points,n);
+    for i = n:-1:1       
+            ClutterStruct(i).thetaClutter = thetaClutter(:,i);
+            ClutterStruct(i).rhoClutter = rhoClutter(:,i);              
+            ClutterStruct(i).rangeRateClutter = rangeRateClutter(:,i);
     end
